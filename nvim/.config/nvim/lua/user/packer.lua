06d2b60dc6404b4f53a64dab720a5cc6253c95aa
Michael Dorst <m@mdorst.net>
2023-01-21 16:30:58 -0800

    nvim: Bootstrap packer

--- nvim/.config/nvim/lua/user/packer.lua
+++ nvim/.config/nvim/lua/user/packer.lua
@@ -1,4 +1,16 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
+-- Install packer if it is not already installed
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
@@ -71,4 +83,9 @@ return require('packer').startup(function(use)
             -- {'rafamadriz/friendly-snippets'}, -- Optional
         }
     }
+
+    -- If packer was just installed, sync
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
