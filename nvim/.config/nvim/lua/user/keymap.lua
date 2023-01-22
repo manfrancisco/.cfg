6d4add4e06929abfbfc0f0f45f417b1de0612c21
Michael Dorst <m@mdorst.net>
2023-01-21 22:57:36 -0800

    nvim: Add keymaps for split navigation, reformat keymap.lua

--- nvim/.config/nvim/lua/user/keymap.lua
+++ nvim/.config/nvim/lua/user/keymap.lua
@@ -1,48 +1,54 @@
-vim.g.mapleader = " "
+vim.g.mapleader = ' '
 
-vim.keymap.set("i", "jk", "<esc>")
-vim.keymap.set("i", "kj", "<esc>")
+vim.keymap.set('i', 'jk', '<esc>')
+vim.keymap.set('i', 'kj', '<esc>')
 
 -- Open netrw
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+
+-- Move between splits
+vim.keymap.set('n', '<leader>j', '<C-w>j')
+vim.keymap.set('n', '<leader>k', '<C-w>k')
+vim.keymap.set('n', '<leader>h', '<C-w>h')
+vim.keymap.set('n', '<leader>l', '<C-w>l')
 
 -- Move highlighted lines up or down
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set('v', 'J', ':m \'>+1<CR>gv=gv')
+vim.keymap.set('v', 'K', ':m \'<-2<CR>gv=gv')
 
 -- Keep cursor in the middle when half-page jumping
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
 
 -- Keep search terms in the middle of the screen
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzv")
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzv')
 
 -- Paste over something without losing your paste buffer
-vim.keymap.set("x", "<leader>p", "\"_dP")
+vim.keymap.set('x', '<leader>p', '"_dP')
 
 -- Copy/cut to system clipboard
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>Y", "\"+Y")
-vim.keymap.set("n", "<leader>d", "\"+d")
-vim.keymap.set("v", "<leader>d", "\"+d")
+vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('v', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>Y', '"+Y')
+vim.keymap.set('n', '<leader>d', '"+d')
+vim.keymap.set('v', '<leader>d', '"+d')
 
 -- Get rid of Ex mode - instead, run the last macro you recorded
-vim.keymap.set("n", "Q", "@@")
+vim.keymap.set('n', 'Q', '@@')
 
 -- Format your buffer
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
 
 -- Search and replace word under cursor
-vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+vim.keymap.set('n', '<leader>s', ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>')
 
 -- Make furrent file executable
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', { silent = true })
 
 -- Redo
-vim.keymap.set("n", "R", "<C-r>")
+vim.keymap.set('n', 'R', '<C-r>')
 
 -- Stay in visual mode after indent
-vim.keymap.set("v", ">", ">gv")
-vim.keymap.set("v", "<", "<gv")
+vim.keymap.set('v', '>', '>gv')
+vim.keymap.set('v', '<', '<gv')
