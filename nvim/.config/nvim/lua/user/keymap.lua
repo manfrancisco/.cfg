6d3c7b7d33ba9d9e27fc550de544dca7b449a676
Michael Dorst <m@mdorst.net>
2023-01-21 23:24:07 -0800

    nvim: Add terminal mode keybinds

--- nvim/.config/nvim/lua/user/keymap.lua
+++ nvim/.config/nvim/lua/user/keymap.lua
@@ -13,8 +13,8 @@ vim.keymap.set('n', '<leader>h', '<C-w>h')
 vim.keymap.set('n', '<leader>l', '<C-w>l')
 
 -- Move highlighted lines up or down
-vim.keymap.set('v', 'J', ':m \'>+1<CR>gv=gv')
-vim.keymap.set('v', 'K', ':m \'<-2<CR>gv=gv')
+vim.keymap.set('v', 'J', [[:m '>+1<CR>gv=gv]])
+vim.keymap.set('v', 'K', [[:m '<-2<CR>gv=gv]])
 
 -- Keep cursor in the middle when half-page jumping
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
@@ -41,7 +41,7 @@ vim.keymap.set('n', 'Q', '@@')
 vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
 
 -- Search and replace word under cursor
-vim.keymap.set('n', '<leader>s', ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>')
+vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- Make furrent file executable
 vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', { silent = true })
@@ -52,3 +52,8 @@ vim.keymap.set('n', 'R', '<C-r>')
 -- Stay in visual mode after indent
 vim.keymap.set('v', '>', '>gv')
 vim.keymap.set('v', '<', '<gv')
+
+-- Terminal mode - go to Normal mode
+vim.keymap.set('t', '<esc>', [[<C-\><C-N>]])
+vim.keymap.set('t', 'jk', [[<C-\><C-N>]])
+vim.keymap.set('t', 'kj', [[<C-\><C-N>]])
