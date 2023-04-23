419b949afa44bdfb8806e070e8a0b6967444e14c
Michael Dorst <m@mdorst.net>
2023-04-23 16:55:59 -0700

    nvim: Wrap to 99 characters

--- nvim/.config/nvim/lua/user/options.lua
+++ nvim/.config/nvim/lua/user/options.lua
@@ -26,6 +26,7 @@ local options = {
     expandtab = true,                        -- convert tabs to spaces
     shiftwidth = 4,                          -- the number of spaces inserted for each indentation
     tabstop = 4,                             -- insert 2 spaces for a tab
+    textwidth = 99,
     cursorline = true,                       -- highlight the current line
     number = true,                           -- set numbered lines
     relativenumber = true,                   -- set relative numbered lines
