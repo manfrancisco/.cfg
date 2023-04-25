118fbc71189b190749d11be9d54c8aabaff1173e
Michael Dorst <m@mdorst.net>
2023-04-25 00:09:12 -0700

    nvim: Move harpoon keybinds into keymap.lua

--- nvim/.config/nvim/lua/user/keymap.lua
+++ nvim/.config/nvim/lua/user/keymap.lua
@@ -93,3 +93,20 @@ else
     -- Open netrw
     vim.keymap.set('n', '<leader>f', vim.cmd.Ex)
 end
+
+-- Harpoon
+local mark_ok, mark = pcall(require, 'harpoon.mark')
+local ui_ok, ui = pcall(require, 'harpoon.ui')
+if mark_ok and ui_ok then
+    vim.keymap.set('n', '<leader>a', mark.add_file)
+    vim.keymap.set('n', '<leader>vh', ui.toggle_quick_menu)
+
+    vim.keymap.set('n', '<leader>1', function() ui.nav_file(1) end)
+    vim.keymap.set('n', '<leader>2', function() ui.nav_file(2) end)
+    vim.keymap.set('n', '<leader>3', function() ui.nav_file(3) end)
+    vim.keymap.set('n', '<leader>4', function() ui.nav_file(4) end)
+    vim.keymap.set('n', '<leader>5', function() ui.nav_file(5) end)
+    vim.keymap.set('n', '<leader>6', function() ui.nav_file(6) end)
+    vim.keymap.set('n', '<leader>7', function() ui.nav_file(7) end)
+    vim.keymap.set('n', '<leader>8', function() ui.nav_file(8) end)
+end
