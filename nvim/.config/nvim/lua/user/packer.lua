8117ad0897de07299d8021d8a8f2ba956a6bbe6d
Michael Dorst <m@mdorst.net>
2023-04-26 18:51:58 -0700

    nvim: Unpin harpoon version, bump treesitter version

--- nvim/.config/nvim/lua/user/packer.lua
+++ nvim/.config/nvim/lua/user/packer.lua
@@ -20,14 +20,14 @@ return require('packer').startup(function(use)
     use {'wbthomason/packer.nvim', commit = '1d0cf98a561f7fd654c970c49f917d74fafe1530'}
 
     -- Navigation
-    use {'theprimeagen/harpoon', commit = '21d0d1bfa3000e4384740bfaefa0ebc51c773786'}
+    use {'theprimeagen/harpoon'}
     use {'nvim-telescope/telescope.nvim', tag = '0.1.1', requires = {'nvim-lua/plenary.nvim'}}
     -- Sets project root
     -- use {'ahmedkhalf/project.nvim', commit = '685bc8e3890d2feb07ccf919522c97f7d33b94e4'}
 
     -- Editing
     use {'Pocco81/auto-save.nvim', commit = '979b6c82f60cfa80f4cf437d77446d0ded0addf0'}
-    use {'nvim-treesitter/nvim-treesitter', tag = 'v0.8.1'}
+    use {'nvim-treesitter/nvim-treesitter', tag = 'v0.9.0'}
     -- Sows the surrounding context in deeply nested code
     use {
         'nvim-treesitter/nvim-treesitter-context',
