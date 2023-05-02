31ccc95216f4ddea16272e5679d303f5e514b173
Michael Dorst <m@mdorst.net>
2023-05-02 15:36:05 -0700

    nvim: Replace config with kickstart.nvim

--- nvim/.config/nvim/after/plugin/colors.lua
+++
@@ -1,5 +0,0 @@
-local ok, _ = pcall(require, 'tokyonight')
-if not ok then return end
-
-vim.cmd.colorscheme('tokyonight-storm')
-
--- nvim/.config/nvim/after/plugin/indent-blankline.lua
+++
@@ -1,7 +0,0 @@
-local ok, indent_blankline = pcall(require, 'indent_blankline')
-if not ok then return end
-
-indent_blankline.setup {
-    show_current_context = true,
-    show_current_context_start = true,
-}
