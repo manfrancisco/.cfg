4dd09a39014823ea6e7de7a168616e6c4145cf18
Michael Dorst <m@mdorst.net>
2024-01-02 15:31:53 -0800

    nix: Move hyprland config to home-manager (really this time)

--- nix/common/hyprland.nix
+++ nix/common/hyprland.nix
@@ -2,34 +2,9 @@
 let
   inherit (lib) mkDefault mkOverride;
 in {
-
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-  };
-
   hardware = {
     opengl.enable = true;
     # Most wayland compositors need this
     nvidia.modesetting.enable = true;
   };
-
-  environment.systemPackages = with pkgs; [
-    inotify-tools
-    joypixels
-    pavucontrol
-    pulseaudio
-    psmisc # killall
-    swaybg
-    waybar
-    wl-clipboard
-    wofi
-  ];
-
-  fonts.packages = with pkgs; [
-    (nerdfonts.override { fonts = [ "FantasqueSansMono" "FiraCode" ]; })
-  ];
-
-  nixpkgs.config.joypixels.acceptLicense = true;
-
 }
--- nix/home/common/hyprland/default.nix
+++ nix/home/common/hyprland/default.nix
@@ -1,7 +1,27 @@
-{ ... }:
+{ pkgs, ... }:
 {
   wayland.windowManager.hyprland = {
     enable = true;
     extraConfig = builtins.readFile ./hyprland.conf;
+  };
+
+  home.packages = with pkgs; [
+    inotify-tools
+    joypixels
+    (nerdfonts.override { fonts = [ "FantasqueSansMono" "FiraCode" ]; })
+    pavucontrol
+    pulseaudio
+    psmisc # killall
+    swaybg
+    waybar
+    wl-clipboard
+    wofi
+  ];
+
+  fonts.fontconfig.enable = true;
+
+  nixpkgs.config = {
+    allowUnfree = true;
+    joypixels.acceptLicense = true;
   };
 }
