12c3c31f49d61d582828bfa21b80de7b45dcd9ba
Michael Dorst <m@mdorst.net>
2024-01-04 12:58:32 -0800

    hyprland: Add auto-start to .zprofile

--- nix/home/common/hyprland/default.nix
+++ nix/home/common/hyprland/default.nix
@@ -4,17 +4,25 @@
     ../waybar
   ];
 
-  home.packages = with pkgs; [
-    inotify-tools
-    joypixels
-    (nerdfonts.override { fonts = [ "FantasqueSansMono" "FiraCode" ]; })
-    pavucontrol
-    pulseaudio
-    psmisc # killall
-    swaybg
-    wl-clipboard
-    wofi
-  ];
+  home = {
+    packages = with pkgs; [
+      inotify-tools
+      joypixels
+      (nerdfonts.override { fonts = [ "FantasqueSansMono" "FiraCode" ]; })
+      pavucontrol
+      pulseaudio
+      psmisc # killall
+      swaybg
+      wl-clipboard
+      wofi
+    ];
+
+    file.".zprofile".text = ''
+      # This script will be run at first login.
+      # If no graphical session is running and we're on tty1, start Hyrland.
+      [ -z "$DISPLAY" ] && [ "$XDG_VTNR" = 1 ] && Hyprland
+    '';
+  };
 
   fonts.fontconfig.enable = true;
 
