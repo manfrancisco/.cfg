4aaf816deeafaa20d5c40f2af5da959ce458cb42
Michael Dorst <m@mdorst.net>
2024-01-06 15:44:57 -0800

    Rewrite hyprland config in nix

--- nix/home/common/wofi/default.nix
+++ nix/home/common/wofi/default.nix
@@ -3,7 +3,17 @@ let
   configHome = config.xdg.configHome;
 in {
   home = {
-    packages = [ pkgs.wofi ];
+    packages = with pkgs; [
+      wofi
+      (writeShellScriptBin "wofi-launch" ''
+        ${psmisc}/bin/killall -q wofi
+        # Wait until the processes have been shut down
+        while pgrep -x wofi >/dev/null; do sleep 1; done
+
+        ${wofi}/bin/wofi --show=drun --lines=5 --prompt="" --hide-scroll --insensitive --columns=2
+      '')
+    ];
+
     file."${configHome}/wofi/style.css".source = ./cfg/style.css;
   };
 }
--- nix/home/nixos-desktop.nix
+++ nix/home/nixos-desktop.nix
@@ -3,6 +3,21 @@
   imports = [
     ./common
     ./common/desktop
-    ./common/hyprland
+    ./common/hyprland.nix
   ];
+
+  wayland.windowManager.hyprland.settings = {
+    monitor = [
+      "DP-1, 1920x1080, 0x0, 1"
+      "HDMI-A-1, 1920x1080, 1920x0, 1"
+    ];
+    workspace = [
+      "1, monitor:DP-1"
+      "2, monitor:DP-1"
+      "3, monitor:DP-1"
+      "4, monitor:HDMI-A-1"
+      "5, monitor:HDMI-A-1"
+      "6, monitor:HDMI-A-1"
+    ];
+  };
 }
