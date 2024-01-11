f2385a94759b51bc33727de7038b0376171fd117
Michael Dorst <m@mdorst.net>
2024-01-10 18:31:23 -0800

    Fix internet problem for nixos-home-server

--- nix/nixos/common/ssh-luks.nix
+++ nix/nixos/common/ssh-luks.nix
@@ -28,7 +28,11 @@
     # Replace `enp8s0` and `Wired connection 1` below as appropriate.
     #   nmcli con down enp8s0
     #   nmcli con mod "Wired connection 1" connection.interface-name enp8s0
+    #   nmcli con up "Wired connection 1"
+    # If still not working, try toggling `autoconnect`.
     #   nmcli con mod "Wired connection 1" connection.autoconnect yes
     kernelParams = [ "ip=dhcp" ];
   };
+  # Without this, NetworkManager will start the wrong connection at boot.
+  networking.interfaces.enp8s0.useDHCP = true;
 }
