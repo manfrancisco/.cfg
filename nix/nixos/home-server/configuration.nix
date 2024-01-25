40f9ab2c34afa3afb71d8518a67478fa2f4c9b4f
Michael Dorst <m@mdorst.net>
2024-01-25 03:07:26 -0800

    nixos-home-server: Get rid of hardware-configuration.nix

--- nix/nixos/home-server/configuration.nix
+++ nix/nixos/home-server/configuration.nix
@@ -1,19 +1,16 @@
 { ... }:
 {
-  imports =
-    [
-      ./hardware-configuration.nix
-      ../common/sh.nix
-      ../common/sops.nix
-      ../common/tailscale.nix
-      ../common/ssh-luks.nix
-    ];
+  imports = [
+    ../common/sh.nix
+    ../common/sops.nix
+    ../common/tailscale.nix
+    ../common/ssh-luks.nix
+  ];
 
-  networking.hostName = "nixos-home-server";
-
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  networking.networkmanager.enable = true;
+  networking = {
+    hostName = "nixos-home-server";
+    networkmanager.enable = true;
+  };
 
   services.openssh = {
     enable = true;
@@ -35,40 +32,46 @@
 
   home-manager.users.me = import ../../home/nixos-home-server.nix;
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
   time.timeZone = "America/Los_Angeles";
 
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
+  boot = {
+    initrd = {
+      availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
+      kernelModules = [ ];
+      luks.devices."root".device = "/dev/disk/by-uuid/a4422541-92d3-4c39-8a04-8d06479bd716";
+    };
+    kernelModules = [ "kvm-amd" ];
+    extraModulePackages = [ ];
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
   };
 
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/c2a26166-1cef-4611-9984-22e390190788";
+      fsType = "ext4";
+    };
+    "/boot" = {
+      device = "/dev/disk/by-uuid/79CE-3C58";
+      fsType = "vfat";
+    };
+  };
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  swapDevices = [ ];
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  hardware = {
+    enableRedistributableFirmware = true;
+    cpu.amd.updateMicrocode = true;
+  };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+  nixpkgs = {
+    config.allowUnfree = true;
+    hostPlatform = "x86_64-linux";
+  };
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  system.stateVersion = "23.11";
 }
